2911a5c0f1e345652093486560e04973553bc3a8
Vitalii Popovych <109319234+vitaliipp@users.noreply.github.com>
2023-08-07 20:52:33 -0500

    mimic asynchronous data fetching

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem('savedTodoList')) || []
-  );
+  const [todoList, setTodoList] = useState([]);
+  useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [],
+          },
+        });
+      }, 2000);
+    }).then((result) => {
+      setTodoList(result.data.todoList);
+    });
+  }, []);
   useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList]);
